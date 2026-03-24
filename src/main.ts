ad2ea533a8b6526d4e20ea2dd3d6ab6fe1adca17
sherine touchene <sherinetouchene20@gmail.com>
2026-03-24 14:22:49 +0100

    Besoin 2 de la V1

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+// Fichier main.ts
+import './style.css';
+
 interface Plat {
     id : number;
     nom : string; 
@@ -23,9 +26,35 @@ const plat: Plat[] =[
     nom : "Emmental 23cm", 
     prix : 7.9000 ,
     description : "sauce tomate premium, origan, huile d'olive extra vierge, emmental, basilic, olive",
+},
+{
+    id : 4,
+    nom : "Margherita 23cm", 
+    prix : 7.9000,
+    description : "sauce tomate premium, origan, huile d'olive extra vierge, mozzarella",
 }
 ]
 
+function init(): void{
+    const app = document.querySelector("#App");
+    if (app) {
+        app.innerHTML = `
+        <div class="menu-container">
+            ${plat
+                .map(
+                    (p) => `
+                    <div class="card">
+                        <h3>${p.nom}</h3>
+                        <p>${p.description}</p>
+                        <p><strong>Prix : ${p.prix.toFixed(2)} €</strong></p>
+                    </div>
+                `
+                )
+                .join("")}
+        </div>
+        `;
+    }
+}
 
-
+init();
 console.log(plat)
