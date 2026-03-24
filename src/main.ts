0756f75f0af40e8fe8c3003f914428ba3c05a3b8
sherine touchene <sherinetouchene20@gmail.com>
2026-03-24 15:22:59 +0100

    Besoin 1/2 de la V2

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,50 @@
 // Fichier main.ts
 import './style.css';
 
-interface Plat {
+interface PlatDTO {
     id : number;
     nom : string; 
-    prix : number ;
+    prix : string ;
     description : string; 
 }
 
-const plat: Plat[] =[
-{
-    id : 1,
-    nom : "Anchois 23cm", 
-    prix : 7.9000 ,
-    description : "sauce tomate premium, origan, huile d'olive extra vierge, anchois, olive",
-},
-{
-    id : 2,
-    nom : "Anchois 33cm", 
-    prix : 11.9000 ,
-    description : "sauce tomate premium, origan, huile d'olive extra vierge, anchois, olive",
-},
-{
-    id : 3,
-    nom : "Emmental 23cm", 
-    prix : 7.9000 ,
-    description : "sauce tomate premium, origan, huile d'olive extra vierge, emmental, basilic, olive",
-},
-{
-    id : 4,
-    nom : "Margherita 23cm", 
-    prix : 7.9000,
-    description : "sauce tomate premium, origan, huile d'olive extra vierge, mozzarella",
-}
-]
+//const plat: Plat[] =[
+//{
+    //id : 1,
+    //nom : "Anchois 23cm", 
+    //prix : 7.9000 ,
+    //description : "sauce tomate premium, origan, huile d'olive extra vierge, anchois, olive",
+//},
+//{
+    //id : 2,
+    //nom : "Anchois 33cm", 
+    //prix : 11.9000 ,
+    //description : "sauce tomate premium, origan, huile d'olive extra vierge, anchois, olive",
+//},
+//{
+    //id : 3,
+    //nom : "Emmental 23cm", 
+    //prix : 7.9000 ,
+    //description : "sauce tomate premium, origan, huile d'olive extra vierge, emmental, basilic, olive",
+//},
+//{
+    //id : 4,
+    //nom : "Margherita 23cm", 
+    //prix : 7.9000,
+    //description : "sauce tomate premium, origan, huile d'olive extra vierge, mozzarella",
+//}
+//]
 
-function init(): void{
-    const app = document.querySelector("#App");
+async function chargerDonnées(): Promise<PlatDTO[]> { 
+const res = await fetch('http://localhost/EatSmart-Sherine/sherine-api-eatsmart/articles'); 
+return await res.json();  
+} 
+
+async function init(): Promise<void> {
+    const app = document.querySelector("#app");
+    // On récupère les données du serveur
+    const plat = await chargerDonnées();
+    console.log(plat);
     if (app) {
         app.innerHTML = `
         <div class="menu-container">
@@ -46,7 +54,7 @@ function init(): void{
                     <div class="card">
                         <h3>${p.nom}</h3>
                         <p>${p.description}</p>
-                        <p><strong>Prix : ${p.prix.toFixed(2)} €</strong></p>
+                        <p><strong>Prix : ${Number(p.prix).toFixed(2)} €</strong></p>
                     </div>
                 `
                 )
@@ -57,4 +65,3 @@ function init(): void{
 }
 
 init();
-console.log(plat)
